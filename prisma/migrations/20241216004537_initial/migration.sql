982d2bb3de703a7122eec2182fddeebb3dfd8ee7
JulianHuertasUNAL <jhuertasd@unal.edu.co>
2024-12-15 21:34:27 -0500

    Endpoints método GET para vivienda, persona, departamento y municipio

--- prisma/migrations/20241216004537_initial/migration.sql
+++ prisma/migrations/20241216004537_initial/migration.sql
@@ -1,8 +1,8 @@
 -- CreateTable
 CREATE TABLE "Vivienda" (
     "id" SERIAL NOT NULL,
-    "Dirección" TEXT NOT NULL,
-    "Tamaño" INTEGER NOT NULL,
+    "direccion" VARCHAR(45) NOT NULL,
+    "tamano" INTEGER NOT NULL,
     "pisos" INTEGER NOT NULL,
 
     CONSTRAINT "Vivienda_pkey" PRIMARY KEY ("id")
@@ -10,10 +10,10 @@ CREATE TABLE "Vivienda" (
 
 -- CreateTable
 CREATE TABLE "Departamento" (
-    "nombre" TEXT NOT NULL,
-    "Área" DOUBLE PRECISION NOT NULL,
-    "Población" INTEGER NOT NULL,
-    "gobernador" TEXT NOT NULL,
+    "nombre" VARCHAR(30) NOT NULL,
+    "area" DOUBLE PRECISION NOT NULL,
+    "poblacion" INTEGER NOT NULL,
+    "gobernador" VARCHAR(60) NOT NULL,
 
     CONSTRAINT "Departamento_pkey" PRIMARY KEY ("nombre")
 );
@@ -21,10 +21,10 @@ CREATE TABLE "Departamento" (
 -- CreateTable
 CREATE TABLE "Municipio" (
     "id" SERIAL NOT NULL,
-    "nombre" TEXT NOT NULL,
-    "Área" DOUBLE PRECISION NOT NULL,
-    "Población" INTEGER NOT NULL,
-    "alcalde" TEXT NOT NULL,
+    "nombre" VARCHAR(30) NOT NULL,
+    "area" DOUBLE PRECISION NOT NULL,
+    "poblacion" INTEGER NOT NULL,
+    "alcalde" VARCHAR(60) NOT NULL,
     "departamentoNombre" TEXT NOT NULL,
 
     CONSTRAINT "Municipio_pkey" PRIMARY KEY ("id")
@@ -33,9 +33,9 @@ CREATE TABLE "Municipio" (
 -- CreateTable
 CREATE TABLE "Persona" (
     "id" INTEGER NOT NULL,
-    "Nombre_completo" TEXT NOT NULL,
-    "Teléfono" TEXT NOT NULL,
-    "Fecha_nacimiento" TIMESTAMP(3) NOT NULL,
+    "nombre" VARCHAR(60) NOT NULL,
+    "telefono" VARCHAR(10) NOT NULL,
+    "fechaNacimiento" TIMESTAMP(3) NOT NULL,
     "genero" INTEGER,
     "viviendaId" INTEGER NOT NULL,
     "municipioId" INTEGER NOT NULL,
@@ -63,16 +63,16 @@ CREATE TABLE "Persona_has_Vivienda" (
 ALTER TABLE "Municipio" ADD CONSTRAINT "Municipio_departamentoNombre_fkey" FOREIGN KEY ("departamentoNombre") REFERENCES "Departamento"("nombre") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Persona" ADD CONSTRAINT "Persona_viviendaId_fkey" FOREIGN KEY ("viviendaId") REFERENCES "Vivienda"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
 ALTER TABLE "Persona" ADD CONSTRAINT "Persona_municipioId_fkey" FOREIGN KEY ("municipioId") REFERENCES "Municipio"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Dependientes" ADD CONSTRAINT "Dependientes_responsableId_fkey" FOREIGN KEY ("responsableId") REFERENCES "Persona"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Persona" ADD CONSTRAINT "Persona_viviendaId_fkey" FOREIGN KEY ("viviendaId") REFERENCES "Vivienda"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Dependientes" ADD CONSTRAINT "Dependientes_dependienteId_fkey" FOREIGN KEY ("dependienteId") REFERENCES "Persona"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Dependientes" ADD CONSTRAINT "Dependientes_responsableId_fkey" FOREIGN KEY ("responsableId") REFERENCES "Persona"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Persona_has_Vivienda" ADD CONSTRAINT "Persona_has_Vivienda_personaId_fkey" FOREIGN KEY ("personaId") REFERENCES "Persona"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
