828c131ff4ae55cad7574be88a42294cfb70a459
cesarArthuroLemos <clemos@unal.edu.co>
2024-12-17 18:54:19 -0500

    formik_submit_handle

--- src/components/atoms/5.ModalUpdate.tsx
+++ src/components/atoms/5.ModalUpdate.tsx
@@ -27,15 +27,24 @@ export default function ModalUpdate(
 
     console.log(initVals)
 
-    const formik = useFormik({initialValues: initVals})
-    console.log(formik)
+    const onSubmit = () => {
+        console.log(formik.values)
+    }
     
+
+    const formik = useFormik({
+        initialValues: initVals,
+        onSubmit
+    })
+
+    console.log(formik)
+
     return (
         <>
             <h2 className="text-xl font-bold mb-4 text-center">
                 Actualiza el Registro
             </h2> 
-            <form>
+            <form onSubmit={formik.handleSubmit}>
             <div className="grid rounded-md">
                 {Object.keys(headers).map((attr) => (
                 <div
@@ -52,7 +61,7 @@ export default function ModalUpdate(
                             type={getFormType(headers[attr].type)}
                             id={attr}
                             placeholder={entry[attr]}
-                            value={formik.values.attr}
+                            value={formik.values[attr]}
                             onChange={formik.handleChange}
                             />                            
                         ) : (
@@ -62,22 +71,24 @@ export default function ModalUpdate(
                     </div>
                 </div>
                 ))}
-            </div>
-            </form>   
-            <div className="flex justify-center gap-6">
-                <button
-                onClick={confirmAction}
-                className={`bg-yellow-400 hover:bg-yellow-500 text-gray-500 px-4 py-2 rounded`}
-                >
-                    Actualizar
-                </button>
-                <button
-                onClick={cancelAction}
-                className="bg-gray-300 text-gray-700 px-4 py-2 rounded hover:bg-gray-400"                        
-                >                            
-                    Cancelar
-                </button>                  
-            </div>                       
+            </div>                
+                <div className="flex justify-center gap-6">
+                    <button
+                        type="submit"
+                        //onClick={confirmAction}
+                        className={`bg-yellow-400 hover:bg-yellow-500 text-gray-500 px-4 py-2 rounded`}
+                    >
+                        Actualizar
+                    </button>
+                    <button
+                        type="button"
+                        onClick={cancelAction}
+                        className="bg-gray-300 text-gray-700 px-4 py-2 rounded hover:bg-gray-400"                        
+                    >                            
+                        Cancelar
+                    </button>                  
+                </div>    
+            </form>                   
         </>
     );
 }
